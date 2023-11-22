369f78b12197162c23c709fcfe015e1b6e058f7e
Saïd <20957603+ayewo@users.noreply.github.com>
2023-11-22 16:59:57 +0100

    Testing removal of new tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -276,182 +276,182 @@ mod init {
     thread \'main\' panicked at \'called `Result::unwrap()` on an `Err` value: IO(Custom { kind: NotConnected, error: \"not a terminal\" })\', src/cli/tc.rs:59:6
     note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
   */
-  #[test]
-  fn test_folder_missing() -> Result<(), Box<dyn std::error::Error>> {
-    println!("BINARY PATH: {}", cargo_bin_str("tailcall"));
-    let mut p = rexpect::spawn(&format!("{} init tmp0", cargo_bin_str("tailcall")), Some(500000))?;
-    let mut res = p.exp_regex(r#".*Do you want to add a file to the project\?.*"#)?;
-    println!("PROMPT: {:?}", res);
+  // #[test]
+  // fn test_folder_missing() -> Result<(), Box<dyn std::error::Error>> {
+  //   println!("BINARY PATH: {}", cargo_bin_str("tailcall"));
+  //   let mut p = rexpect::spawn(&format!("{} init tmp0", cargo_bin_str("tailcall")), Some(500000))?;
+  //   let mut res = p.exp_regex(r#".*Do you want to add a file to the project\?.*"#)?;
+  //   println!("PROMPT: {:?}", res);
 
-    p.send("N\n")?; // send the newline in tests because it can't do the readline checks that it would in a proper terminal
-    res = p.exp_regex(".*No such file or directory.*")?;
-    println!("OUTPUT after sending 'N': {:?}", res);
+  //   p.send("N\n")?; // send the newline in tests because it can't do the readline checks that it would in a proper terminal
+  //   res = p.exp_regex(".*No such file or directory.*")?;
+  //   println!("OUTPUT after sending 'N': {:?}", res);
 
-    Ok(())
-  }
+  //   Ok(())
+  // }
 
-  #[test]
-  #[serial]
-  fn test_folder_nonexistent() -> Result<(), rexpect::error::Error> {
-    let mut p = rexpect::spawn(&format!("{} init tmp0", cargo_bin_str("tailcall")), Some(500000))?;
-    let mut res = p.exp_regex(r#".*Do you want to add a file to the project\?.*"#)?;
-    println!("PROMPT: {:?}", res);
+  // #[test]
+  // #[serial]
+  // fn test_folder_nonexistent() -> Result<(), rexpect::error::Error> {
+  //   let mut p = rexpect::spawn(&format!("{} init tmp0", cargo_bin_str("tailcall")), Some(500000))?;
+  //   let mut res = p.exp_regex(r#".*Do you want to add a file to the project\?.*"#)?;
+  //   println!("PROMPT: {:?}", res);
 
-    p.send("N\n")?; // send the newline in tests because it can't do the readline checks that it would in a proper terminal
-    res = p.exp_regex(".*No such file or directory.*")?;
-    println!("OUTPUT after sending 'N': {:?}", res);
+  //   p.send("N\n")?; // send the newline in tests because it can't do the readline checks that it would in a proper terminal
+  //   res = p.exp_regex(".*No such file or directory.*")?;
+  //   println!("OUTPUT after sending 'N': {:?}", res);
 
-    Ok(())
-  }
+  //   Ok(())
+  // }
 
-  #[allow(clippy::too_many_arguments)]
-  fn test_nonexistent_folder_and_file(
-    folder_name: &str,
-    answer1: &str,
-    answer2: &str,
-    answer3: &str,
-  ) -> Result<(), rexpect::error::Error> {
-    let folder_name = &folder_to_path_cwd(folder_name);
-    let mut p = rexpect::spawn(
-      &format!("{} init {}", cargo_bin_str("tailcall"), folder_name),
-      Some(5000),
-    )?;
+  // #[allow(clippy::too_many_arguments)]
+  // fn test_nonexistent_folder_and_file(
+  //   folder_name: &str,
+  //   answer1: &str,
+  //   answer2: &str,
+  //   answer3: &str,
+  // ) -> Result<(), rexpect::error::Error> {
+  //   let folder_name = &folder_to_path_cwd(folder_name);
+  //   let mut p = rexpect::spawn(
+  //     &format!("{} init {}", cargo_bin_str("tailcall"), folder_name),
+  //     Some(5000),
+  //   )?;
 
-    let res1 = p.exp_regex(r#".*Do you want to add a file to the project\?.*"#)?;
-    println!("PROMPT: {:?}", res1);
-    p.send(&format!("{}\n", answer1))?;
+  //   let res1 = p.exp_regex(r#".*Do you want to add a file to the project\?.*"#)?;
+  //   println!("PROMPT: {:?}", res1);
+  //   p.send(&format!("{}\n", answer1))?;
 
-    let res2 = p.exp_regex(".*Enter the file name.*")?;
-    println!("OUTPUT after sending '{}' to 1st prompt: {:?}", answer1, res2);
-    p.send_line(&format!("{}\n", answer2))?;
+  //   let res2 = p.exp_regex(".*Enter the file name.*")?;
+  //   println!("OUTPUT after sending '{}' to 1st prompt: {:?}", answer1, res2);
+  //   p.send_line(&format!("{}\n", answer2))?;
 
-    let res3 = p.exp_regex(".*Do you want to create the file.*")?;
-    p.send_line(&format!("{}\n", answer3))?;
-    println!("OUTPUT after sending '{}' to 2nd prompt: {:?}", answer2, res3);
+  //   let res3 = p.exp_regex(".*Do you want to create the file.*")?;
+  //   p.send_line(&format!("{}\n", answer3))?;
+  //   println!("OUTPUT after sending '{}' to 2nd prompt: {:?}", answer2, res3);
 
-    if !folder_exists(folder_name) {
-      let res4 = p.exp_regex(".*No such file or directory.*")?;
-      println!("OUTPUT after sending '{}' to 3rd prompt: {:?}", answer3, res4);
-    } else {
-      let res4 = p.exp_regex(r#".*.*"#)?;
-      println!("OUTPUT after sending '{}' to 3rd prompt: {:?}", answer3, res4);
+  //   if !folder_exists(folder_name) {
+  //     let res4 = p.exp_regex(".*No such file or directory.*")?;
+  //     println!("OUTPUT after sending '{}' to 3rd prompt: {:?}", answer3, res4);
+  //   } else {
+  //     let res4 = p.exp_regex(r#".*.*"#)?;
+  //     println!("OUTPUT after sending '{}' to 3rd prompt: {:?}", answer3, res4);
 
-      std::thread::sleep(std::time::Duration::from_secs(2)); // wait for the spawned process to write to disk before proceeding
+  //     std::thread::sleep(std::time::Duration::from_secs(2)); // wait for the spawned process to write to disk before proceeding
 
-      if answer3.eq_ignore_ascii_case("y") {
-        // assert the other files were created
-        assert!(
-          file_exists_in_folder(folder_name, answer2),
-          "File does not exist in the folder."
-        );
-        assert!(
-          file_exists_in_folder(folder_name, ".graphqlrc.yml"),
-          "File does not exist in the folder."
-        );
-      }
-      // assert the file was created
-      assert!(folder_exists(folder_name), "Folder does not exist.");
-      assert!(
-        file_exists_in_folder(folder_name, ".tailcallrc.graphql"),
-        "File does not exist in the folder."
-      );
-    }
+  //     if answer3.eq_ignore_ascii_case("y") {
+  //       // assert the other files were created
+  //       assert!(
+  //         file_exists_in_folder(folder_name, answer2),
+  //         "File does not exist in the folder."
+  //       );
+  //       assert!(
+  //         file_exists_in_folder(folder_name, ".graphqlrc.yml"),
+  //         "File does not exist in the folder."
+  //       );
+  //     }
+  //     // assert the file was created
+  //     assert!(folder_exists(folder_name), "Folder does not exist.");
+  //     assert!(
+  //       file_exists_in_folder(folder_name, ".tailcallrc.graphql"),
+  //       "File does not exist in the folder."
+  //     );
+  //   }
 
-    Ok(())
-  }
+  //   Ok(())
+  // }
 
-  #[test]
-  #[serial]
-  fn test_folder_nonexistent2() -> Result<(), rexpect::error::Error> {
-    return test_nonexistent_folder_and_file("tmp0", "y", "test", "n");
-  }
+  // #[test]
+  // #[serial]
+  // fn test_folder_nonexistent2() -> Result<(), rexpect::error::Error> {
+  //   return test_nonexistent_folder_and_file("tmp0", "y", "test", "n");
+  // }
 
-  #[test]
-  #[serial]
-  fn test_folder_nonexistent3() -> Result<(), rexpect::error::Error> {
-    return test_nonexistent_folder_and_file("tmp0", "y", "test", "y");
-  }
+  // #[test]
+  // #[serial]
+  // fn test_folder_nonexistent3() -> Result<(), rexpect::error::Error> {
+  //   return test_nonexistent_folder_and_file("tmp0", "y", "test", "y");
+  // }
 
-  #[test]
-  #[serial]
-  fn test_folder_exists() -> Result<(), rexpect::error::Error> {
-    let folder_name = "tmp01";
-    create_folder(folder_name);
-    let _ = test_nonexistent_folder_and_file(folder_name, "y", "test1.graphql", "n");
-    delete_folder(folder_name);
+  // #[test]
+  // #[serial]
+  // fn test_folder_exists() -> Result<(), rexpect::error::Error> {
+  //   let folder_name = "tmp01";
+  //   create_folder(folder_name);
+  //   let _ = test_nonexistent_folder_and_file(folder_name, "y", "test1.graphql", "n");
+  //   delete_folder(folder_name);
 
-    let folder_name = "tmp02";
-    create_folder(folder_name);
-    let _ = test_nonexistent_folder_and_file(folder_name, "y", "test2.graphql", "y");
-    delete_folder(folder_name);
+  //   let folder_name = "tmp02";
+  //   create_folder(folder_name);
+  //   let _ = test_nonexistent_folder_and_file(folder_name, "y", "test2.graphql", "y");
+  //   delete_folder(folder_name);
 
-    Ok(())
-  }
+  //   Ok(())
+  // }
 
-  fn folder_exists(folder_name: &str) -> bool {
-    let path = std::path::Path::new(folder_name);
-    path.is_dir()
-  }
+  // fn folder_exists(folder_name: &str) -> bool {
+  //   let path = std::path::Path::new(folder_name);
+  //   path.is_dir()
+  // }
 
-  fn folder_to_path_cwd(folder_name: &str) -> String {
-    let current_dir = env::current_dir().expect("Failed to get current directory");
-    let file_path = current_dir.join(folder_name);
+  // fn folder_to_path_cwd(folder_name: &str) -> String {
+  //   let current_dir = env::current_dir().expect("Failed to get current directory");
+  //   let file_path = current_dir.join(folder_name);
 
-    return file_path.to_string_lossy().into_owned();
-  }
+  //   return file_path.to_string_lossy().into_owned();
+  // }
 
-  fn file_exists_in_folder(folder_path: &str, file_name: &str) -> bool {
-    let file_path = PathBuf::from(folder_path).join(file_name);
-    fs::metadata(file_path).is_ok()
-  }
+  // fn file_exists_in_folder(folder_path: &str, file_name: &str) -> bool {
+  //   let file_path = PathBuf::from(folder_path).join(file_name);
+  //   fs::metadata(file_path).is_ok()
+  // }
 
-  fn create_folder(folder_name: &str) -> bool {
-    match fs::create_dir(folder_name) {
-      Ok(_) => {
-        println!("Folder '{}' created successfully.", folder_name);
-        true
-      }
-      Err(e) => {
-        eprintln!("Error creating folder '{}': {}", folder_name, e);
-        false
-      }
-    }
-  }
+  // fn create_folder(folder_name: &str) -> bool {
+  //   match fs::create_dir(folder_name) {
+  //     Ok(_) => {
+  //       println!("Folder '{}' created successfully.", folder_name);
+  //       true
+  //     }
+  //     Err(e) => {
+  //       eprintln!("Error creating folder '{}': {}", folder_name, e);
+  //       false
+  //     }
+  //   }
+  // }
 
-  fn delete_folder(folder_name: &str) -> bool {
-    match fs::remove_dir_all(folder_name) {
-      Ok(_) => {
-        println!("Folder '{}' deleted successfully.", folder_name);
-        true
-      }
-      Err(e) => {
-        eprintln!("Error deleting folder '{}': {}", folder_name, e);
-        false
-      }
-    }
-  }
+  // fn delete_folder(folder_name: &str) -> bool {
+  //   match fs::remove_dir_all(folder_name) {
+  //     Ok(_) => {
+  //       println!("Folder '{}' deleted successfully.", folder_name);
+  //       true
+  //     }
+  //     Err(e) => {
+  //       eprintln!("Error deleting folder '{}': {}", folder_name, e);
+  //       false
+  //     }
+  //   }
+  // }
 
-  fn cargo_bin_str(name: &str) -> String {
-    let env_var = format!("CARGO_BIN_EXE_{}", name);
-    return std::env::var_os(env_var)
-      .map(|p| p.into())
-      .unwrap_or_else(|| target_dir().join(format!("{}{}", name, env::consts::EXE_SUFFIX)))
-      .to_string_lossy()
-      .into();
-  }
+  // fn cargo_bin_str(name: &str) -> String {
+  //   let env_var = format!("CARGO_BIN_EXE_{}", name);
+  //   return std::env::var_os(env_var)
+  //     .map(|p| p.into())
+  //     .unwrap_or_else(|| target_dir().join(format!("{}{}", name, env::consts::EXE_SUFFIX)))
+  //     .to_string_lossy()
+  //     .into();
+  // }
 
-  // Adapted from
-  // https://github.com/rust-lang/cargo/blob/485670b3983b52289a2f353d589c57fae2f60f82/tests/testsuite/support/mod.rs#L507
-  fn target_dir() -> path::PathBuf {
-    env::current_exe()
-      .ok()
-      .map(|mut path| {
-        path.pop();
-        if path.ends_with("deps") {
-          path.pop();
-        }
-        path
-      })
-      .unwrap()
-  }
+  // // Adapted from
+  // // https://github.com/rust-lang/cargo/blob/485670b3983b52289a2f353d589c57fae2f60f82/tests/testsuite/support/mod.rs#L507
+  // fn target_dir() -> path::PathBuf {
+  //   env::current_exe()
+  //     .ok()
+  //     .map(|mut path| {
+  //       path.pop();
+  //       if path.ends_with("deps") {
+  //         path.pop();
+  //       }
+  //       path
+  //     })
+  //     .unwrap()
+  // }
 }
